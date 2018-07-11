1e1377bb70df9ec6dff0708709ff8e79782b5c78
Elijah Smith <elijahs2106@gmail.com>
2018-07-10 22:47:58 -0400

    Create GoogleMaps component that will used for API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, NavLink, Switch } from "react-router-dom";
 
 import './App.css';
 import Github from './Github';
+import GoogleMaps from './GoogleMaps';
 import Homework from './Homework';
 
 class App extends Component {
@@ -16,11 +17,15 @@ class App extends Component {
 
         <ul className="navLinks">
           <li>
-            <NavLink to="/github">GitHub APIP</NavLink>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+          <li>
+            <NavLink to="/googlemaps">GoogleMaps API</NavLink>
           </li>
         </ul>
         <Switch>
           <Route path="/github" component={Github} />
+          <Route path="/googlemaps" component={GoogleMaps} />
           <Route path="/homework" component={Homework} />
           <Route render={() => (
             <p>To get started, click one of the links above.</p>
---
+++ src/GoogleMaps.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import './GoogleMaps.css';
+
+class GoogleMaps extends Component {
+    state = {
+        city: '',
+    }
+
+    handleChange = (e) => {
+        this.setState({ city: e.target.value })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.history.push(`/googlemaps/${this.state.city}`);
+    }
+
+    render() {
+        return (
+            <div className="GoogleMaps">
+                <img
+                    src="https://cdn.colorlib.com/wp/wp-content/uploads/sites/2/google-maps-wordpress-plugins.png"
+                    alt="GoogleMaps"
+                    className="logo"
+                />
+
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <input 
+                            required
+                            autoFocus
+                            type="text"
+                            value={this.state.city}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">
+                            Look up City
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default GoogleMaps;
